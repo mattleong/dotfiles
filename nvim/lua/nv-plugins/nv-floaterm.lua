5a5a0a7e7917bcb21637754beb0b3488b230f5fe
Matt Leong <mattleong91@gmail.com>
2021-07-07 17:45:30 -0700

    Init telescope

--- nvim/lua/nv-plugins/nv-floaterm.lua
+++ nvim/lua/nv-plugins/nv-floaterm.lua
@@ -7,7 +7,7 @@ g.floaterm_height = 0.8
 g.floaterm_title = '|terminal($1/$2)|'
 g.floaterm_opener = 'vsplit'
 
-cmd 'hi FloatermBorder guibg=None guifg=#7aa2f7'
+cmd 'hi FloatermBorder guibg=None guifg=#3d59a1'
 
 map('n', '<C-l>', ':FloatermToggle<CR>', { noremap = true })
 map('t', '<C-l>', [[<C-\><C-n>]], { noremap = true })
