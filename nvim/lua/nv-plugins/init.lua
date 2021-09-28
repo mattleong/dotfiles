8483fd5486da4b7f180b32576e84da2dadd2b76c
Matt Leong <mattleong91@gmail.com>
2021-09-27 20:11:08 -0700

    clean up plugins/init.lua

--- nvim/lua/nv-plugins/init.lua
+++ nvim/lua/nv-plugins/init.lua
@@ -4,119 +4,105 @@ local fn = vim.fn
 -- Automatically install packer.nvim
 local install_path = fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
 if fn.empty(fn.glob(install_path)) > 0 then
-	cmd('!git clone https://github.com/wbthomason/packer.nvim ' .. install_path)
+  cmd('!git clone https://github.com/wbthomason/packer.nvim ' .. install_path)
 end
 
 local packer = require 'packer'
 local use = packer.use
 
 packer.startup(
-	function()
-		use 'wbthomason/packer.nvim'
+  function()
+    use 'wbthomason/packer.nvim'
 
-		use 'kyazdani42/nvim-web-devicons'
-		use 'kyazdani42/nvim-tree.lua'
+    use 'kyazdani42/nvim-web-devicons'
+    use 'kyazdani42/nvim-tree.lua'
 
-		-- lang/syntax stuff
-		use 'ChristianChiarulli/vim-solidity'
-		use {
-			'nvim-treesitter/nvim-treesitter',
-			run = ':TSUpdate',
-			requires = {
-				'windwp/nvim-ts-autotag',
-				'JoosepAlviste/nvim-ts-context-commentstring',
-			},
-		}
+    -- lang/syntax stuff
+    use 'ChristianChiarulli/vim-solidity'
+    use {
+      'nvim-treesitter/nvim-treesitter',
+      run = ':TSUpdate',
+      requires = {
+        'windwp/nvim-ts-autotag',
+        'JoosepAlviste/nvim-ts-context-commentstring',
+      },
+    }
 
-		-- theme stuff
-		use {
-			'NTBBloodbath/galaxyline.nvim',
-			branch = 'main',
-			requires = {
-				'kyazdani42/nvim-web-devicons',
-				opt = true
-			},
-		}
+    -- git
+    use 'tpope/vim-fugitive'
+    use 'tpope/vim-rhubarb'
+    use {
+      'lewis6991/gitsigns.nvim',
+      requires = { 'nvim-lua/plenary.nvim' },
+      config = function()
+        require('gitsigns').setup()
+      end
+    }
 
-		use {
-			'folke/tokyonight.nvim',
-			config = function()
-				vim.g.tokyonight_style = 'night'
-				vim.g.tokyonight_sidebars = { "qf", "packer" }
-				vim.cmd 'color tokyonight'
-			end,
-		}
+    -- theme stuff
+    use {
+      'NTBBloodbath/galaxyline.nvim',
+      branch = 'main',
+--      requires = {
+--        'kyazdani42/nvim-web-devicons',
+--        opt = true
+--      },
+    }
 
-		-- floating terminal
-		use {
-			'voldikss/vim-floaterm',
-			opt = true,
-			cmd = { 'FloatermToggle', 'FloatermNew', 'FloatermSend', },
-		}
+    use {
+      'folke/tokyonight.nvim',
+      config = function()
+        vim.g.tokyonight_style = 'night'
+        vim.g.tokyonight_sidebars = { "qf", "packer" }
+        vim.cmd 'color tokyonight'
+      end,
+    }
 
-		use {
-			'nvim-telescope/telescope.nvim',
-			requires = {
-				'nvim-lua/popup.nvim',
-				'nvim-lua/plenary.nvim',
-			},
-		}
+    -- floating terminal
+    use {
+      'voldikss/vim-floaterm',
+      opt = true,
+      cmd = { 'FloatermToggle', 'FloatermNew', 'FloatermSend', },
+    }
 
-		-- git
-		use 'tpope/vim-fugitive'
-		use 'tpope/vim-rhubarb'
-		use {
-			'lewis6991/gitsigns.nvim',
-			requires = { 'nvim-lua/plenary.nvim' },
-			config = function()
-				require('gitsigns').setup()
-			end
-		}
+    use {
+      'nvim-telescope/telescope.nvim',
+      requires = {
+        'nvim-lua/popup.nvim',
+        'nvim-lua/plenary.nvim',
+      },
+    }
 
-		-- session management
-		use {
-			'rmagatti/auto-session',
-			config = function()
-				require('auto-session').setup {
-					pre_save_cmds = { 'NvimTreeClose' },
-				}
-			end,
-		}
-		use {
-			'rmagatti/session-lens',
-			requires = {
-				'rmagatti/auto-session',
-				'nvim-telescope/telescope.nvim'
-			},
-			opt = true,
-			cmd = { 'SearchSession' },
-			config = function()
-				require('session-lens').setup {
-					shorten_path = true,
-				}
-			end
-		}
 
-		-- autocomplete/ide
-		use { 'neoclide/coc.nvim', branch = 'release', }
+    -- session management
+    use {
+      'rmagatti/auto-session',
+      config = function()
+        require('auto-session').setup {
+          pre_save_cmds = { 'NvimTreeClose' },
+        }
+      end,
+    }
 
-		-- colorized hex codes
-		use {
-			'norcalli/nvim-colorizer.lua',
-			opt = true,
-			cmd = { 'ColorizerToggle', },
-			config = function()
-				require'colorizer'.setup()
-			end
-		}
+    -- autocomplete/ide
+    use { 'neoclide/coc.nvim', branch = 'release', }
 
-	end
+    -- colorized hex codes
+    use {
+      'norcalli/nvim-colorizer.lua',
+      opt = true,
+      cmd = { 'ColorizerToggle', },
+      config = function()
+        require'colorizer'.setup()
+      end
+    }
+
+  end
 )
 
 require 'nv-plugins.nvimtree'.init()
 require 'nv-plugins.telescope'.init()
 require 'nv-plugins.treesitter'.init()
-require 'nv-plugins.nvimtree'
 require 'nv-plugins.galaxyline'
 require 'nv-plugins.floaterm'
 cmd 'source ~/.config/nvim/lua/nv-plugins/coc.vim'
