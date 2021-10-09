1dcc0f9b4327abfdb61fe1cfabbb2b70375bb284
Matt Leong <mattleong91@gmail.com>
2021-10-08 18:11:55 -0700

    feat(mappings): add quickfix maps

--- nvim/lua/nv-mappings.lua
+++ nvim/lua/nv-mappings.lua
@@ -1,3 +1,10 @@
 local g = vim.g
+local map = require('nv-utils').map
 
 g.mapleader = ' '
+
+-- Quickfix
+map('n', '<leader>ck', ':cexpr []<CR>', { noremap = true })
+map('n', '<leader>cc', ':cclose <CR>', { noremap = true })
+map('n', '<leader>co', ':copen <CR>', { noremap = true })
+map('n', '<leader>cf', ':cfdo %s/', { noremap = true })
--- nvim/lua/nv-plugins/telescope.lua
+++ nvim/lua/nv-plugins/telescope.lua
@@ -5,11 +5,10 @@ local colors = require('nv-plugins.colors')
 local M = {}
 
 local function set_telescope_mapping()
-  map('n', '<leader>f', ':lua require("telescope.builtin").find_files()<CR>', { noremap = true })
+  map('n', '<leader>p', ':lua require("telescope.builtin").find_files()<CR>', { noremap = true })
+  map('n', '<leader>f', ':lua require("telescope.builtin").git_files()<CR>', { noremap = true })
   map('n', '<leader>k', ':lua require("telescope.builtin").buffers()<CR>', { noremap = true })
   map('n', '<leader>s', ':lua require("telescope.builtin").live_grep()<CR>', { noremap = true })
-  map('n', '<leader>q', ':lua require("telescope.builtin").quickfix()<CR>', { noremap = true })
-  map('n', '<leader>gg', ':lua require("telescope.builtin").git_files()<CR>', { noremap = true })
   map('n', '<leader>gc', ':lua require("telescope.builtin").git_commits()<CR>', { noremap = true })
   map('n', '<leader>gs', ':lua require("telescope.builtin").git_status()<CR>', { noremap = true })
   map('n', '<leader>x', ':SearchSession<CR>', { noremap = true })
