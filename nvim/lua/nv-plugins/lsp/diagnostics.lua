d3d76219d9adccd4ae6ccc4b829678a7716bd0b0
Matt Leong <mattleong91@gmail.com>
2021-10-10 16:15:18 -0700

    diag icons started, need to fix highlights

--- nvim/lua/nv-plugins/lsp/diagnostics.lua
+++ nvim/lua/nv-plugins/lsp/diagnostics.lua
@@ -2,7 +2,7 @@ require("trouble").setup {}
 
 local signs = { Error = " ", Warning = " ", Hint = " ", Information = " " }
 for type, icon in pairs(signs) do
-  local hl = "LspDiagnosticsSign" .. type
+  local hl = "DiagnosticSign" .. type
   vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
 end
 
@@ -10,7 +10,11 @@ vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
   vim.lsp.diagnostic.on_publish_diagnostics, {
     underline = false,
     update_in_insert = false,
-    virtual_text = true,
+    virtual_text = {
+      spacing = 3,
+--      prefix = '👾',
+    },
+--     severity_sort = {reverse = true},
     signs = true,
   }
 )
