3d70774aa010452dcb575f7cc5769d0ac16b151e
Matt Leong <mattleong91@gmail.com>
2021-10-14 15:43:09 -0700

    messing with lsp underline colors

--- nvim/lua/nv-core/theme/highlights.lua
+++ nvim/lua/nv-core/theme/highlights.lua
@@ -13,18 +13,23 @@ function M.lsp()
   highlight('LspDiagnosticsSignError', 'None', colors.error)
   highlight('LspDiagnosticsSignWarning', 'None', colors.warn)
   highlight('LspDiagnosticsSignInformation', 'None', colors.info)
-  highlight('LspDiagnosticsSignHint', 'None', colors.teal)
+  highlight('LspDiagnosticsSignHint', 'None', colors.hint)
+
+  highlight('DiagnosticUnderlineError', 'None', colors.error)
+  highlight('DiagnosticUnderlineWarning', 'None', colors.warn)
+  highlight('DiagnosticUnderlineInformation', 'None', colors.info)
+  highlight('DiagnosticUnderlineHint', 'None', colors.hint)
 
 
   -- error colors
   highlight('LspDiagnosticsError', 'None', colors.error)
   highlight('LspDiagnosticsWarn', 'None', colors.warn)
   highlight('LspDiagnosticsInfo', 'None', colors.info)
-  highlight('LspDiagnosticsHint', 'None', colors.teal)
+  highlight('LspDiagnosticsHint', 'None', colors.hint)
   highlight('DiagnosticError', 'None', colors.error)
   highlight('DiagnosticWarn', 'None', colors.warn)
   highlight('DiagnosticInfo', 'None', colors.info)
-  highlight('DiagnosticHint', 'None', colors.teal)
+  highlight('DiagnosticHint', 'None', colors.hint)
 
   -- signature highlight color
   highlight('LspSignatureActiveParameter', 'None', colors.orange)
@@ -33,7 +38,7 @@ function M.lsp()
   highlight('LspSagaDocTruncateLine', 'NormalFloat', colors.floatBorder)
   highlight('LspSagaDiagnosticTruncateLine', 'NormalFloat', colors.floatBorder)
   highlight('LspSagaCodeActionTruncateLine', 'NormalFloat', colors.floatBorder)
-  highlight('LspSagaRenamePromptPrefix', 'NormalFloat', colors.teal)
+  highlight('LspSagaRenamePromptPrefix', 'NormalFloat', colors.hint)
   highlight('LspSagaDiagnosticHeader', 'NormalFloat', colors.blue)
   vim.cmd [[
     highlight clear NormalFloat
