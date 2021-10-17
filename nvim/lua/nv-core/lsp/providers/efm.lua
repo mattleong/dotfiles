a723a981388870491eb714ed6b525b4f79542513
Matt Leong <mattleong91@gmail.com>
2021-10-16 18:12:09 -0700

    Lua formatter

--- nvim/lua/nv-core/lsp/providers/efm.lua
+++ nvim/lua/nv-core/lsp/providers/efm.lua
@@ -1,22 +1,33 @@
 local lspconfig = require('lspconfig')
 
-local prettier = { formatCommand = 'prettier --stdin-filepath ${INPUT}', formatStdin = true }
+local stylua = {
+	formatCommand = 'stylua -s --quote-style AutoPreferSingle -',
+	formatStdin = true,
+}
+
+local prettier = {
+	--  formatCommand = 'prettier --stdin-filepath ${INPUT}',
+	formatCommand = 'prettierd "${INPUT}"',
+	formatStdin = true,
+}
+
 local filetypes = {
-  css = { prettier },
-  html = { prettier },
-  javascript = { prettier },
-  javascriptreact = { prettier },
-  json = { prettier },
-  markdown = { prettier },
-  scss = { prettier },
-  typescript = { prettier },
-  typescriptreact = { prettier },
-  yaml = { prettier },
+	css = { prettier },
+	html = { prettier },
+	lua = { stylua },
+	javascript = { prettier },
+	javascriptreact = { prettier },
+	json = { prettier },
+	markdown = { prettier },
+	scss = { prettier },
+	typescript = { prettier },
+	typescriptreact = { prettier },
+	yaml = { prettier },
 }
 
 return {
-  init_options = { documentFormatting = true, codeAction = true },
-  root_dir = lspconfig.util.root_pattern { '.git/', '.' },
-  filetypes = vim.tbl_keys(filetypes),
-  settings = { languages = filetypes },
+	init_options = { documentFormatting = true, codeAction = true },
+	root_dir = lspconfig.util.root_pattern({ '.git/', '.' }),
+	filetypes = vim.tbl_keys(filetypes),
+	settings = { languages = filetypes },
 }
